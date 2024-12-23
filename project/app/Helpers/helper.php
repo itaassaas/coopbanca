f80627f365ad724650b0568624a4d52aaf812a2f
itaassaas <54752417+itaassaas@users.noreply.github.com>
2024-12-23 16:53:43 -0500

    ERROR VOLVIMOS

--- project/app/Helpers/helper.php
+++ project/app/Helpers/helper.php
@@ -5,22 +5,19 @@ use App\Models\Generalsetting;
 
 
 
-if(!function_exists('showPrice')){
-  function showPrice($price, $currency){
-      $gs = Generalsetting::first();
+  if(!function_exists('showPrice')){
       
-      // Calculate price with currency value and round to 2 decimals
-      $price = round(($price) * $currency->value, 2);
-      
-      // Format number with thousand separator and 2 decimal places
-      $formatted_price = number_format($price, 2, '.', ',');
-      
-      // Return based on currency format setting
-      if($gs->currency_format == 0){
-          return $currency->sign . $formatted_price;
-      } else {
-          return $formatted_price . $currency->sign;
-      }
+      function showPrice($price,$currency){
+        $gs = Generalsetting::first();
+        
+        $price = round(($price) * $currency->value,2);
+        if($gs->currency_format == 0){
+            return $currency->sign. $price;
+        }
+        else{
+            return $price. $currency->sign;
+        }
+    }
   }
   
 
