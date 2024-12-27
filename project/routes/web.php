b5b3ea5b0811df21c5fbff961413882bb16f456b
itaassaas <54752417+itaassaas@users.noreply.github.com>
2024-12-27 14:19:44 -0500

    Update web.php

--- project/routes/web.php
+++ project/routes/web.php
@@ -26,8 +26,4 @@ use Illuminate\Support\Facades\Route;
 
   Route::get('/currency/{id}', 'Frontend\FrontendController@currency')->name('front.currency');
   Route::get('/language/{id}', 'Frontend\FrontendController@language')->name('front.language');
-// In web.php
-Route::post('admin/users/withdraws/reject/{id}', 'Admin\UserController@reject')
-    ->name('admin.withdraw.reject')
-    ->middleware('auth:admin');
 
