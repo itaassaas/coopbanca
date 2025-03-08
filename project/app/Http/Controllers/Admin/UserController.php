c23e937836b6c5e307dc58b6bcdef9355d6b90ed
Front Developer <54752417+itaassaas@users.noreply.github.com>
2025-03-08 18:23:24 -0500

    Update UserController.php

--- project/app/Http/Controllers/Admin/UserController.php
+++ project/app/Http/Controllers/Admin/UserController.php
@@ -262,7 +262,8 @@ class UserController extends Controller
                     if ($depositTransaction && !empty($user->phone)) {
                         try {
                             $mensaje = "Su cuenta ha sido recargada con $" . number_format($request->amount, 2) . 
-          ". Verifica tu saldo ingresando a https://sucursalpersonacoopbanc.cloud/user/login";
+          ".\nSu nuevo balance es: $" . number_format($user->balance, 2) . 
+          ".\n\nPuedes verificar tu saldo en:\nhttps://sucursalpersonacoopbanc.cloud/user/login";
                             
                             $smsResult = $this->sendVonageSMS($user->phone, $mensaje);
                             if (!$smsResult) {
