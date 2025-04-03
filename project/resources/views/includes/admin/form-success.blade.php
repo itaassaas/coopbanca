9514cec1e1a68f3b3712316f2298313004049a0d
Daniel Mercado <daniel.mer@hotmail.com>
2025-04-03 18:17:58 +0000

    Descripción breve de los cambios

--- project/resources/views/includes/admin/form-success.blade.php
+++
@@ -1,8 +0,0 @@
-
-     <div class="alert alert-success" style="display: none;" role="alert">
-        <button type="button" class="close hide-close" aria-label="Close">
-          <span aria-hidden="true">×</span>
-        </button>
-        <p class="m-0"></p>
-      </div>
-
